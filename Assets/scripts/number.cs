1e8ee61ab43e34a6b44776dac21d5809a26ef623
IMAN-00 <imanalawneh928@gmail.com>
2024-10-28 13:59:58 +0200

    important code update!!

--- Assets/scripts/number.cs
+++ Assets/scripts/number.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class number : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+     void Start()
+{
+    int counter = 0;
+    while (counter < 20)
     {
-        Debug.Log 
+        int randomNumber = Random.Range(1,21);
+       Debug.Log(randomNumber);
+       counter++;
+       if (randomNumber == 5) continue;
+       if (randomNumber == 15) break;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+}
+
+
+
 }
